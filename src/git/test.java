bc54ba05faedfcc619d75a108828ccefd6bd974d
lrc <979946744@qq.com>
2020-06-07 12:27:19 +0800

    test2

--- src/git/test.java
+++ src/git/test.java
@@ -3,6 +3,6 @@ package git;
 public class test {
 	public static void main(String[] args) {
 		System.out.println("独孤九剑");
-		System.out.println("独孤九剑----git2");
+		System.out.println("独孤九剑----git22");
 	}
 }
